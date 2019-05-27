9e5b7786919c43f3cc98c9447b96d50b70008037
metroid112 <federico.aguilera112@gmail.com>
2019-05-27 17:50:47 -0300

    more commands

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         for chan in self.get_all_channels():
-            if chan.name == 'general':
+            if chan.name == 'bot_pato':
                 await chan.send(f'Inicializando SGR...')
                 await chan.send(f'Explotando el servidor de fuentes...')
                 await chan.send(f'Tirando los ambientes de argentina...')
@@ -18,8 +18,13 @@ class MyClient(discord.Client):
             print(f'It\'s a command!')
             if mes[1:5] == 'play':
                 await message.channel.send(f'Reproduciendo video: {mes[6:]}')
-            if mes[1:] == 'reset' and message.author == 'Metroid#4781':
-                await message.channel.send('Reseteando bot..')
+            if mes[1:5] == 'exit':
+                await message.channel.send(f'Cerrando bot...')
+                await self.close()
+            if mes[1:6] == 'reset':
+                await message.channel.send(f'Reseteando bot...')
+                self.clear()
+                await self.start('NTgyNTc1OTc2MTkwNTc0NTk3.XOv1Cw.Tz2X0OzrNjK4NXB4sh6NjSD99pU', bot=True)
         print(f'Message from {message.author}: {message.content}')
 
 
