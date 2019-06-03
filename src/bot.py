6fb635d94feda435eba0b3edd63644111915d1b5
metroid112 <federico.aguilera112@gmail.com>
2019-06-03 11:04:50 -0300

    creando personaje

--- src/bot.py
+++ src/bot.py
@@ -10,43 +10,75 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 
+def is_valid_character_name(name):
+    valid_characters = set('qwertyuiopasdfghjklzxcvbnm')
+    if all((character.lower() in valid_characters) for character in name):
+        return True
+    else:
+        return False
+
+
 def sql(sentence):
     cursor_temp = db_connection.cursor()
     print(sentence)
     query = cursor_temp.execute(sentence).fetchall()
-    cursor_temp.close()
-    return query
-
-
-def insert_sql(table, args):
-    cursor_temp = db_connection.cursor()
-    print(args)
-    sentence = f'INSERT INTO {table} VALUES (?'
-    for i in range(len(args) - 1):
-        sentence += ',?'
-    sentence += ')'
-    print(sentence)
-    cursor_temp.execute(sentence, args)
     db_connection.commit()
     cursor_temp.close()
+    return query
 
 
 async def create_command(message):
     query_user = sql(f'SELECT * FROM USERS WHERE USER_NAME = "{message.author.name}"')
     print(query_user)
-    if len(query_user) == 0:
+    if not query_user:
+        await message.channel.send(f'Usuario {message.author.name} no existe, creando...')
         await create_user(message)
-    # await self.create_character()
+    else:
+        user_id = query_user[0][0]
+        await create_character(message, user_id)
 
 
 async def create_user(message):
     query_id = sql(f'SELECT USER_ID FROM USERS')
-    if len(query_id) == 0:
+    print(query_id)
+    if not query_id:
         user_id = 1
     else:
-        user_id = query_id[len(query_id)][0]
-    insert_sql('USERS', [user_id, message.author.name])
-    await message.channel.send(f'Usuario nuevo ({user_id}, {message.author.name}), creando...')
+        user_id = query_id[-1][0]
+    sql(f'INSERT INTO USERS (USER_ID, USER_NAME) VALUES {user_id}, {message.author.name}')
+    await message.channel.send(f'Usuario nuevo ({user_id}, {message.author.name})')
+
+
+async def create_character(message, user_id):
+    if is_valid_character_name(message.content[8:]):
+        character_name = message.content[8:].strip()
+        query_character = sql(f'SELECT * FROM CHARACTERS WHERE USER_ID = {user_id} AND CHARACTER_NAME = "{character_name}"')
+        print(query_character)
+        if not query_character:
+            await message.channel.send(f'Personaje {character_name} no existe, creando...')
+            query_character = sql(f'SELECT CHARACTER_ID FROM CHARACTERS')
+            print(query_character)
+            if not query_character:
+                character_id = 1
+            else:
+                character_id = query_character[-1][0]
+            sql(f'INSERT INTO CHARACTERS (USER_ID, CHARACTER_ID, CHARACTER_NAME) VALUES ({user_id}, {character_id}, "{character_name}")')
+        else:
+            await message.channel.send(f'Usuario {message.author.name} ya tiene un personaje llamado {character_name}')
+    else:
+        await message.channel.send(f'El nombre {message.content[8:]} no es válido')
+
+
+async def play_video(message):
+    request = youtube.search().list(
+        part="snippet",
+        maxResults=1,
+        q=message.content[6:]
+    )
+    response = request.execute()
+    pprint.pprint(response)
+    await message.channel.send(f'Respuesta api youtube: {response}')
+    await message.channel.send(f'Reproduciendo video: {message.content[6:]}')
 
 
 class MyClient(discord.Client):
@@ -61,24 +93,13 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
-        print(message)
-        print(message.channel)
-        print(message.content)
         if isinstance(message.content, str):
             if message.content[0] == '&':
                 print(f'It\'s a command!')
                 if message.content[1:7] == 'create':
                     await create_command(message)
                 if message.content[1:5] == 'play':
-                    request = youtube.search().list(
-                        part="snippet",
-                        maxResults=1,
-                        q=message.content[6:]
-                    )
-                    response = request.execute()
-                    pprint.pprint(response)
-                    await message.channel.send(f'Respuesta api youtube: {response}')
-                    await message.channel.send(f'Reproduciendo video: {message.content[6:]}')
+                    await play_video(message)
                 if message.content[1:5] == 'exit' and message.author.name == 'Metroid':
                     await message.channel.send(f'Cerrando bot...')
                     await self.close()
